97ca02e77c049e9aee2f800264233e69df3ad483
Imoh Precious <jaypee88830@gmail.com>
2024-06-01 01:30:13 +0100

    first commit

---
+++ src/tutorial.ts
@@ -0,0 +1,10 @@
+let awesomeName = 'shakeAndBake'
+awesomeName = awesomeName.toUpperCase();
+console.log(awesomeName)
+
+let amount = 20
+amount = 12 - 1
+console.log(amount)
+
+let isAwesome = true;
+isAwesome = 'shakeAndBake'
